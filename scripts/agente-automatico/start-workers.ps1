941dd28e20c759c8e167a5ab39d9b1c84207ee61
gekali cobranca <gekalicobranca@gmail.com>
2026-09-15 21:56:16 -0300

    Inclui HFlex na inicializacao dos agentes

--- scripts/agente-automatico/start-workers.ps1
+++ scripts/agente-automatico/start-workers.ps1
@@ -6,7 +6,8 @@ $workers = @(
   @{ Script = 'agente:worker:square-guarulhos'; Log = 'square-guarulhos-worker' },
   @{ Script = 'agente:worker:verti'; Log = 'verti-worker' },
   @{ Script = 'agente:worker:lello'; Log = 'lello-worker' },
-  @{ Script = 'agente:worker:atipass'; Log = 'atipass-worker' }
+  @{ Script = 'agente:worker:atipass'; Log = 'atipass-worker' },
+  @{ Script = 'agente:worker:hflex'; Log = 'hflex-worker' }
 )
 
 foreach ($worker in $workers) {
